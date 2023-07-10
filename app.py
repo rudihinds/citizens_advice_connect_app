0ed37d2affacbdc0f50cad1b03470b66d8969f8b
Rudi Hinds <rudihinds@Rudis-MacBook-Pro.local>
2023-07-10 09:36:29 +0100

    init

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import os
 
-import aws_cdk as cdk
+from aws_cdk import core as cdk
 
 from citizens_advice.citizens_advice_stack import CitizensAdviceStack
 
--- citizens_advice/citizens_advice_stack.py
+++ citizens_advice/citizens_advice_stack.py
@@ -1,19 +1,51 @@
 from aws_cdk import (
-    # Duration,
-    Stack,
-    # aws_sqs as sqs,
+    aws_dynamodb as ddb,
+    core,
+    aws_iam as iam,
+    aws_lambda as _lambda,
 )
-from constructs import Construct
 
-class CitizensAdviceStack(Stack):
+class CitizensAdviceStack(core.Stack):
 
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+    def __init__(self, scope: core.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "CitizensAdviceQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        role = iam.Role(self, 'LambdaRole',
+            assumed_by=iam.ServicePrincipal('lambda.amazonaws.com'),
+            # managed_policies=[
+            #     iam.ManagedPolicy.from_aws_managed_policy_name('service-role/AWSLambdaBasicExecutionRole')
+            # ]
+        )
+
+        policy = iam.PolicyStatement(
+            actions=[
+                "dynamodb:PutItem",
+                "dynamodb:GetItem",
+                "dynamodb:Scan",
+                "dynamodb:Query",
+                "dynamodb:UpdateItem",
+                "dynamodb:DeleteItem",
+            ],
+            resources=[
+                "*"
+            ]   
+        )
+
+        role.add_to_policy(policy)
+        
+        services_table = ddb.Table(
+            self, 'ServicesTable',
+            partition_key=ddb.Attribute(name='ServiceName', type=ddb.AttributeType.STRING),
+            billing_mode=ddb.BillingMode.PAY_PER_REQUEST,
+            removal_policy=core.RemovalPolicy.DESTROY
+        )
+
+        # The Holidays table
+        holidays_table = ddb.Table(
+            self, 'HolidaysTable',
+            partition_key=ddb.Attribute(name='HolidayDate', type=ddb.AttributeType.STRING),
+            billing_mode=ddb.BillingMode.PAY_PER_REQUEST,
+            removal_policy=core.RemovalPolicy.DESTROY
+        )
+        
