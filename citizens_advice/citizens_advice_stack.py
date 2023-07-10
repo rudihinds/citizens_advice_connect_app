f0a7cc07e0d496b86772d843bab44351c0b895cf
Rudi Hinds <rudihinds@Rudis-MacBook-Pro.local>
2023-07-10 10:32:58 +0100

    add lambda to stack, add policy to lambda for dynamodb actions

--- citizens_advice/citizens_advice_stack.py
+++ citizens_advice/citizens_advice_stack.py
@@ -10,7 +10,23 @@ class CitizensAdviceStack(core.Stack):
     def __init__(self, scope: core.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
+        # The Services table
+        services_table = ddb.Table(
+            self, 'ServicesTable',
+            partition_key=ddb.Attribute(name='ServiceName', type=ddb.AttributeType.STRING),
+            billing_mode=ddb.BillingMode.PAY_PER_REQUEST,
+            removal_policy=core.RemovalPolicy.DESTROY
+        )
 
+        # The Holidays table
+        holidays_table = ddb.Table(
+            self, 'HolidaysTable',
+            partition_key=ddb.Attribute(name='HolidayDate', type=ddb.AttributeType.STRING),
+            billing_mode=ddb.BillingMode.PAY_PER_REQUEST,
+            removal_policy=core.RemovalPolicy.DESTROY
+        )
+
+        # The Lambda role
         role = iam.Role(self, 'LambdaRole',
             assumed_by=iam.ServicePrincipal('lambda.amazonaws.com'),
             # managed_policies=[
@@ -18,6 +34,7 @@ class CitizensAdviceStack(core.Stack):
             # ]
         )
 
+        # The Lambda role permissions
         policy = iam.PolicyStatement(
             actions=[
                 "dynamodb:PutItem",
@@ -34,18 +51,15 @@ class CitizensAdviceStack(core.Stack):
 
         role.add_to_policy(policy)
         
-        services_table = ddb.Table(
-            self, 'ServicesTable',
-            partition_key=ddb.Attribute(name='ServiceName', type=ddb.AttributeType.STRING),
-            billing_mode=ddb.BillingMode.PAY_PER_REQUEST,
-            removal_policy=core.RemovalPolicy.DESTROY
-        )
-
-        # The Holidays table
-        holidays_table = ddb.Table(
-            self, 'HolidaysTable',
-            partition_key=ddb.Attribute(name='HolidayDate', type=ddb.AttributeType.STRING),
-            billing_mode=ddb.BillingMode.PAY_PER_REQUEST,
-            removal_policy=core.RemovalPolicy.DESTROY
+        # The Lambda function
+        lambda_function = _lambda.Function(self, 'OpeningHoursFunction',
+            runtime=_lambda.Runtime.PYTHON_3_8,
+            code=_lambda.Code.asset('lambda'),
+            handler='opening_hours.lambda_handler',
+            role=role,
+            environment={
+                'SERVICES_TABLE': services_table.table_name,
+                'HOLIDAYS_TABLE': holidays_table.table_name
+            }
         )
         
